bd24563a8997249448755e6f0fbbc8be0dda74e4
dilan besong <besongdilan@gmail.com>
2023-10-21 20:30:03 +0100

    fix loggIn  session issue

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ import bodyParser from "body-parser";
 
 const connectedUsers = []
 //const activeFriends = []
-
+ 
 const MONGO_URI =  process.env.MONGO_URI || "mongodb://localhost:27017/CEEDB";
 console.log(MONGO_URI);
 mongoose.connect(MONGO_URI)
