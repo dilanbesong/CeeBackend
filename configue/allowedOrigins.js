537b36c5074a4d92eb9656e61478d7b219378114
dilan besong <besongdilan@gmail.com>
2023-10-26 21:09:29 +0100

    fixed cors issue

--- configue/allowedOrigins.js
+++ configue/allowedOrigins.js
@@ -1,8 +1,11 @@
 const allowedOrigins = [
   "*",
   "https://cee-react-template.vercel.app",
+  "https://cee-react-template.vercel.app/",
   "https://www.ceeinfo.netlify.app",
+  "https://www.ceeinfo.netlify.app/",
   "http://localhost:5173",
+  "http://localhost:5173/",
 ];
 
 export { allowedOrigins }
